e1faa5d763dc0798f9d1dcd01e9722d7d40bee75
Simon Hamp <simon.hamp@me.com>
2024-01-01 23:24:37 +0000

    Add test case

---
+++ tests/test.php
@@ -0,0 +1,15 @@
+<?php
+
+use SimonHamp\TheOg\Image;
+use SimonHamp\TheOg\Background;
+
+include_once __DIR__.'/../vendor/autoload.php';
+
+(new Image())
+    ->accentColor('#cc0000')
+    ->border()
+    ->url('https://example.com/blog/some-blog-post-url')
+    ->title('Some blog post title that is quite big and quite long')
+    ->description('Some slightly smaller but potentially much longer subtext. It could be really long so we might need to trim it completely after many words')
+    ->background(Background::JustWaves)
+    ->save(__DIR__.'/test.png');
