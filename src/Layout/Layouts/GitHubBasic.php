803f0d58d85d90fd250b2d6efaa7746e024371c0
Simon Hamp <simon.hamp@me.com>
2024-01-12 00:57:29 +0000

    Use the improved layout API

--- src/Layout/Layouts/GitHubBasic.php
+++ src/Layout/Layouts/GitHubBasic.php
@@ -5,6 +5,7 @@ namespace SimonHamp\TheOg\Layout\Layouts;
 use SimonHamp\TheOg\BorderPosition;
 use SimonHamp\TheOg\Layout\AbstractLayout;
 use SimonHamp\TheOg\Layout\Position;
+use SimonHamp\TheOg\Layout\TextBox;
 
 class GitHubBasic extends AbstractLayout
 {
@@ -14,49 +15,66 @@ class GitHubBasic extends AbstractLayout
     protected int $padding = 20;
     protected int $width = 1280;
 
-    protected function features(string $feature, string $setting): mixed
+    public function features(): void
     {
-        $settings = [
-            'call_to_action' => [
-                'font_size' => 20,
-                'dimensions' => [$this->mountArea()->box->width(), 240],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 20,
-                    'relativeTo' => fn () => $this->getDescription(),
-                ],
-            ],
-            'description' => [
-                'font_size' => 40,
-                'dimensions' => [$this->mountArea()->box->width(), 240],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 50,
-                    'relativeTo' => fn () => $this->getTitle(),
-                    'position' => Position::BottomLeft
-                ],
-            ],
-            'title' => [
-                'font_size' => 60,
-                'dimensions' => [$this->mountArea()->box->width(), 400],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 20,
-                    'relativeTo' => fn () => $this->getUrl(),
-                    'position' => Position::BottomLeft
-                ],
-            ],
-            'url' => [
-                'font_size' => 28,
-                'dimensions' => [$this->mountArea()->box->width(), 45],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 20,
-                    'relativeTo' => fn () => $this->mountArea(),
-                ],
-            ],
-        ];
+        $this->addFeature('title', (new TextBox())
+            ->text($this->title())
+            ->color($this->config->theme->getTitleColor())
+            ->font($this->config->theme->getTitleFont())
+            ->size(60)
+            ->box($this->mountArea()->box->width(), 400)
+            ->position(
+                x: 0,
+                y: 20,
+                relativeTo: fn () => $this->getFeature('url'),
+                position: Position::BottomLeft
+            )
+        );
 
-        return $settings[$feature][$setting];
+        if ($description = $this->description()) {
+            $this->addFeature('description', (new TextBox())
+                ->text($description)
+                ->color($this->config->theme->getDescriptionColor())
+                ->font($this->config->theme->getDescriptionFont())
+                ->size(40)
+                ->box($this->mountArea()->box->width(), 240)
+                ->position(
+                    x: 0,
+                    y: 50,
+                    relativeTo: fn() => $this->getFeature('title'),
+                    position: Position::BottomLeft
+                )
+            );
+        }
+
+        if ($callToAction = $this->callToAction()) {
+            $this->addFeature('call_to_action', (new TextBox())
+                ->text($callToAction)
+                ->color($this->config->theme->getCallToActionColor())
+                ->font($this->config->theme->getCallToActionFont())
+                ->size(20)
+                ->box($this->mountArea()->box->width(), 240)
+                ->position(
+                    x: 0,
+                    y: 20,
+                    relativeTo: fn() => $this->getFeature('description')
+                )
+            );
+        }
+
+        if ($url = $this->url()) {
+            $this->addFeature('url', (new TextBox())
+                ->text($url)
+                ->color($this->config->theme->getUrlColor())
+                ->font($this->config->theme->getUrlFont())
+                ->size(28)
+                ->box($this->mountArea()->box->width(), 45)
+                ->position(
+                    x: 0,
+                    y: 20,
+                    relativeTo: fn() => $this->mountArea()
+                )
+            );
+        }
     }
 }
--- src/Layout/Layouts/Standard.php
+++ src/Layout/Layouts/Standard.php
@@ -5,6 +5,7 @@ namespace SimonHamp\TheOg\Layout\Layouts;
 use SimonHamp\TheOg\BorderPosition;
 use SimonHamp\TheOg\Layout\AbstractLayout;
 use SimonHamp\TheOg\Layout\Position;
+use SimonHamp\TheOg\Layout\TextBox;
 
 class Standard extends AbstractLayout
 {
@@ -14,54 +15,71 @@ class Standard extends AbstractLayout
     protected int $padding = 40;
     protected int $width = 1200;
 
+    public function features(): void
+    {
+        $this->addFeature('title', (new TextBox())
+            ->text($this->title())
+            ->color($this->config->theme->getTitleColor())
+            ->font($this->config->theme->getTitleFont())
+            ->size(60)
+            ->box($this->mountArea()->box->width(), 400)
+            ->position(
+                x: 0,
+                y: 10,
+                relativeTo: fn () => $this->getFeature('url'),
+                position: Position::BottomLeft
+            )
+        );
+
+        if ($description = $this->description()) {
+            $this->addFeature('description', (new TextBox())
+                ->text($description)
+                ->color($this->config->theme->getDescriptionColor())
+                ->font($this->config->theme->getDescriptionFont())
+                ->size(40)
+                ->box($this->mountArea()->box->width(), 240)
+                ->position(
+                    x: 0,
+                    y: 50,
+                    relativeTo: fn() => $this->getFeature('title'),
+                    position: Position::BottomLeft
+                )
+            );
+        }
+
+        if ($callToAction = $this->callToAction()) {
+            $this->addFeature('call_to_action', (new TextBox())
+                ->text($callToAction)
+                ->color($this->config->theme->getCallToActionColor())
+                ->font($this->config->theme->getCallToActionFont())
+                ->size(20)
+                ->box($this->mountArea()->box->width(), 240)
+                ->position(
+                    x: 0,
+                    y: 20,
+                    relativeTo: fn() => $this->getFeature('description')
+                )
+            );
+        }
+
+        if ($url = $this->url()) {
+            $this->addFeature('url', (new TextBox())
+                ->text($url)
+                ->color($this->config->theme->getUrlColor())
+                ->font($this->config->theme->getUrlFont())
+                ->size(28)
+                ->box($this->mountArea()->box->width(), 45)
+                ->position(
+                    x: 0,
+                    y: 0,
+                    relativeTo: fn() => $this->mountArea()
+                )
+            );
+        }
+    }
+
     public function url(): string
     {
         return strtoupper(parent::url());
-    }
-
-    protected function features(string $feature, string $setting): mixed
-    {
-        $settings = [
-            'call_to_action' => [
-                'font_size' => 20,
-                'dimensions' => [$this->mountArea()->box->width(), 240],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 20,
-                    'relativeTo' => fn () => $this->getDescription(),
-                ],
-            ],
-            'description' => [
-                'font_size' => 40,
-                'dimensions' => [$this->mountArea()->box->width(), 240],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 50,
-                    'relativeTo' => fn () => $this->getTitle(),
-                    'position' => Position::BottomLeft
-                ],
-            ],
-            'title' => [
-                'font_size' => 60,
-                'dimensions' => [$this->mountArea()->box->width(), 400],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 20,
-                    'relativeTo' => fn () => $this->getUrl(),
-                    'position' => Position::BottomLeft
-                ],
-            ],
-            'url' => [
-                'font_size' => 28,
-                'dimensions' => [$this->mountArea()->box->width(), 45],
-                'layout' => [
-                    'x' => 0,
-                    'y' => 20,
-                    'relativeTo' => fn () => $this->mountArea(),
-                ],
-            ],
-        ];
-
-        return $settings[$feature][$setting];
     }
 }
