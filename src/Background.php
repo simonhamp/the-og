5dbad1cb41f901cb907f4336a0d601474febe290
Simon Hamp <simon.hamp@me.com>
2024-01-15 11:27:52 +0000

    Add SubtlePatterns references

--- src/Background.php
+++ src/Background.php
@@ -7,9 +7,24 @@ use SimonHamp\TheOg\Theme\Background as BaseBackground;
 
 enum Background: string
 {
+    /**
+     * @see https://www.toptal.com/designers/subtlepatterns/bananas/
+     */
     case Bananas = 'bananas.webp';
+
+    /**
+     * @see https://www.toptal.com/designers/subtlepatterns/cloudy-day/
+     */
     case CloudyDay = 'cloudy-day.png';
+
+    /**
+     * @see https://www.toptal.com/designers/subtlepatterns/grid-me/
+     */
     case GridMe = 'gridme.webp';
+
+    /**
+     * @see https://www.toptal.com/designers/subtlepatterns/just-waves/
+     */
     case JustWaves = 'just-waves.webp';
 
     public function load(): BackgroundInterface
